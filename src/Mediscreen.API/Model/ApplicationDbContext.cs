b0c680ca799acf3587b22b5c6a7aa7aa1465423a
Marc <marcpinson9@gmail.com>
2023-01-20 17:29:44 +0100

    Implement patient views & controller

--- src/Mediscreen.API/Model/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-namespace MediscreenAPI.Model.Entities
-{
-    public class ApplicationDbContext : DbContext
-    {
-        public DbSet<Patient> Trades { get; set; }
-    }
-}
---
+++ src/Mediscreen.WebApp/Services/PatientService.cs
@@ -0,0 +1,39 @@
+﻿using Domain.DTOs;
+
+namespace MediscreenWepApp.Services
+{
+    public class PatientService : IPatientService
+    {
+        private readonly HttpClient _httpClient;
+
+        public PatientService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<HttpResponseMessage> Create(PatientDto dto)
+        {
+            return null;
+        }
+
+        public async Task<List<PatientDto>> ReadAll()
+        {
+            return new List<PatientDto>();
+        }
+
+        public async Task<PatientDto> Read(int id)
+        {
+            return new PatientDto();
+        }
+
+        public async Task<HttpResponseMessage> Update(PatientDto dto)
+        {
+            return null;
+        }
+
+        public async Task<HttpResponseMessage> Delete(int id)
+        {
+            return null;
+        }
+    }
+}
